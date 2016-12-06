35e786dc370e7775340f9d3ed6d6b7056da7aafc
theory friction practice <jackson.brebnor@gmail.com>
2016-12-06 15:25:25 -0500

    adds routes and home

---
+++ src/imports/components/home.js
@@ -0,0 +1,12 @@
+import React, { PropTypes, } from 'react';
+
+const Home = ({ children, }) => (
+  <div id="home">
+    <div> HOMEPAGE</div>
+  </div>
+    );
+
+// Home.needs = [ TaskActions.getTasks, ];
+// Home.contextTypes = { muiTheme: React.PropTypes.object, };
+
+export default Home;
---
+++ src/imports/routes.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Route, IndexRoute, } from 'react-router';
+
+// import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Main,Home, } from './components';
+
+// import { Login, Register, } from './components/auth';
+//
+// injectTapEventPlugin();
+//
+// const logChange = (prevState, nextState, replace,) => {
+//   console.log('now changing Route from');
+//   console.log(nextState);
+//   console.log('to');
+//   console.log(nextState);
+// };
+//
+// const requireLogin = store => (nextState, replace, cb) => {
+//   const { auth: { user, }, } = store.getState();
+//   if (!user) {
+//     replace('/');
+//   }
+//   cb();
+// };
+
+const getRoutes = store => (
+  <Route name="app" component={Main} path="/">
+    <IndexRoute component={Home} />
+
+  </Route>
+
+  );
+const root = (<Route name="app" component={Main} path="/">
+  <IndexRoute component={Home} />
+
+</Route>)
+
+// export default getRoutes;
+export default root;
