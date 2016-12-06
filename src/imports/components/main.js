3fd92a09ea1078a992484026d5efdb243d24a827
theory friction practice <jackson.brebnor@gmail.com>
2016-12-06 10:09:14 -0500

    adds main component and eslint

---
+++ src/imports/components/main.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes, } from 'react';
+
+class Main extends Component {
+    render() {
+        return (
+        <div id="main-view" className="container">
+          <p>HELLO</p>
+        </div>
+        );
+    }
+}
+
+
+export default Main;
