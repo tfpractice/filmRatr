4ef52002432d64d96dc324ff50c12a2509e76d10
theory friction practice <jackson.brebnor@gmail.com>
2016-12-06 15:56:10 -0500

    fetchComponentData

---
+++ src/imports/utils/fetch_component_data.js
@@ -0,0 +1,17 @@
+const getNeeds = ({ needs, } = { needs: [], }) => needs;
+
+const isWrapped = ({ WrappedComponent = null, }) => WrappedComponent;
+
+const compNeeds = component => isWrapped(component)
+    ? getNeeds(component.WrappedComponent)
+    : getNeeds(component);
+
+const flatten = (prev = [], next = []) => [ ...prev, ...next, ];
+
+const fetchComponentData = (dispatch, components, params) => {
+  const needs = components.map(compNeeds).reduce(flatten, []);
+  const promises = needs.map(need => Promise.resolve(dispatch(need(params))));
+  return Promise.all(promises);
+};
+
+export default fetchComponentData;
