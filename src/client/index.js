af0dca9cdb7bb9696e04eae314d1b2ebae8bc6e8
theory friction practice <jackson.brebnor@gmail.com>
2016-12-06 10:12:08 -0500

    client/index.js

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, } from 'react-dom';
+
+import { components } from '../imports';
+const { Main }= components;
+
+
+render(<Main />, document.getElementById('root'));
