1e01fc31771ef37a30c6f6047192a257b1d3785e
theory friction practice <tfpractice@theorys-MacBook-Air.local>
2016-12-06 21:34:29 -0500

    adds movie db config

--- src/imports/modules/movies/constants.js
+++ src/imports/modules/movies/constants.js
@@ -1,6 +1,8 @@
 export const API_URL = (typeof window === 'undefined' || process.env.NODE_ENV === 'test')
     ? process.env.BASE_URL || (`http://localhost:${process.env.PORT || 3000}/api`)
     : '/api';
+export const MOVIE_API_URL = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.MOVIE_DB_API_URL}`
+
 
 const PENDING = 'PENDING';
 const SUCCESS = 'SUCCESS';
---
+++ src/imports/utils/movie_db.js
@@ -0,0 +1,4 @@
+export const MOVIE_API_URL ='https://api.themoviedb.org/3/';
+export const MOVIE_DB_API_KEY= process.env.MOVIE_DB_API_KEY;
+export const MOVIE_DB_SEARCH_URL =
+ `${MOVIE_API_URL}/search/movie?api_key=${MOVIE_DB_API_KEY}`
