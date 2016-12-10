2662b7db37b86a354a343891e0379b86c029e373
theory friction practice <jackson.brebnor@gmail.com>
2016-12-09 22:59:07 -0500

    cleans up containers

--- src/imports/components/search/form.js
+++ src/imports/components/search/form.js
@@ -4,18 +4,11 @@ import { connect, } from 'react-redux';
 import FlatButton from 'material-ui/FlatButton';
 import { TextField, } from 'redux-form-material-ui';
 
-const renderField = ({ input, type, id, }) => (
-  <input {...input} type={type} id={id} />
+const SearchForm = ({ handleSubmit, }) => (
+  <form onSubmit={handleSubmit} >
+    <Field name="query" component={TextField} hintText="Movie Title" id="query" type="text" />
+    <FlatButton label="Submit" primary type="submit" />
+  </form>
   );
-
-const SearchForm = ({ handleSubmit, ...props }) => {
-  console.log(props);
-  return (
-    <form onSubmit={handleSubmit} >
-      <Field name="query" component={TextField} hintText="Movie Title" id="query" type="text" />
-      <FlatButton label="Submit" primary type="submit" />
-    </form>
-  );
-};
 
 export default connect()(reduxForm()(SearchForm));
--- src/imports/container.js
+++ src/imports/container.js
@@ -1,5 +1,4 @@
 import React, { Component, } from 'react';
-
 import { AppContainer as HotContainer, } from 'react-hot-loader';
 import { Provider, } from 'react-redux';
 import { browserHistory, Router, } from 'react-router';
