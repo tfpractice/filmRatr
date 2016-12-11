f70a33ffe439abe6e6a4ff8c46fcd729e56c1a1d
theory friction practice <jackson.brebnor@gmail.com>
2016-12-10 20:17:29 -0500

    movie schema

---
+++ src/server/models/reviews.js
@@ -0,0 +1,11 @@
+import mongoose, { Schema, } from 'mongoose';
+
+const ReviewSchema = new Schema({
+  text:      { type: String, default: 'default review text', required: true, },
+  movie_id:  { type: String, default: '9620', required: true, },
+  dateAdded: { type: 'Date', default: Date.now, required: true, },
+  // author: { type: Schema.Types.ObjectId, ref: 'User', default: null, },
+}, { toObject: { virtuals: true, }, toJSON: { virtuals: true, }, });
+
+const Review = mongoose.model('Review', ReviewSchema);
+export default Review;
