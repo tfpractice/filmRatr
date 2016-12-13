680ffbe606faab1f39aab67189d923aff0b04967
theory friction practice <jackson.brebnor@gmail.com>
2016-12-13 10:29:02 -0500

    refactors config/webpack.babel.js

--- config/webpack/config.babel.js
+++ config/webpack/config.babel.js
@@ -1,9 +1,8 @@
-import sharedConf from './shared';
+import shared from './shared';
 import * as actions from './actions';
 import { CONFIG_EVENTS, } from './constants';
 
-const config = (common = sharedConf, event) =>
+const config = (common = shared, event) =>
   CONFIG_EVENTS.has(event) ? actions[event](common) : common;
 
-export default (env, ...args) =>
-(config(sharedConf(env), process.env.npm_lifecycle_event));
+export default env => (config(shared(env), process.env.npm_lifecycle_event));
--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -2,12 +2,11 @@ import webpack from 'webpack';
 import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-// import ManifestPlugin from 'webpack-manifest-plugin';
-// import ChunkManifestPlugin from 'chunk-manifest-webpack-plugin';
-
 import { PATHS, ROOT_PATH, } from './constants';
 
-export default env => ({
+const defEnvar = { prod: true, };
+
+export default (env = defEnvar) => ({
   context: ROOT_PATH,
   entry: { app: PATHS.app, },
   resolve: {
@@ -47,12 +46,6 @@ export default env => ({
   plugins: [
     // new webpack.optimize.CommonsChunkPlugin(
     //   { names: ['vendor', 'manifest',], }),
-
-    // new ManifestPlugin({ basePath: '/', }),
-    // new ChunkManifestPlugin({
-    //   filename: 'chunk-manifest.json',
-    //   manifestVariable: 'webpackManifest',
-    // }),
     new webpack.DefinePlugin({ 'process.env': { NODE_ENV: '"production"', }, }),
     new webpack.LoaderOptionsPlugin({
       minimize: true,
