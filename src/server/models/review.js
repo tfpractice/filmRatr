a2e373e9794a3436a7d0ad99ea88e0c88e2ce73d
theory friction practice <jackson.brebnor@gmail.com>
2016-12-14 17:55:22 -0500

    db topFiveMovies schema method

--- src/server/models/review.js
+++ src/server/models/review.js
@@ -7,6 +7,30 @@ const ReviewSchema = new Schema({
   // author: { type: Schema.Types.ObjectId, ref: 'User', default: null, },
 }, { toObject: { virtuals: true, }, toJSON: { virtuals: true, }, });
 
+ReviewSchema.statics.distinctMovies = function () {
+  return this.distinct('movie_id');
+};
+
+ReviewSchema.statics.countByMovieID = function (movie_id) {
+  this.count({ movie_id, });
+};
+
+ReviewSchema.statics.topFiveMovies = function () {
+  this.aggregate(
+    { $group: { _id: '$movie_id', count: { $sum: 1, }, }, },
+     { $sort: { count: -1, }, },
+     { $limit: 5, },
+  )
+    .then((r) => {
+      console.log('movie_id groups', r);
+
+    // return r.sort
+    });
+};
+
 const Review = mongoose.model('Review', ReviewSchema);
 
+Review.distinctMovies();
+Review.countByMovieID('550');
+Review.topFiveMovies();
 export default Review;
