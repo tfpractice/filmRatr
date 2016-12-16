ea08033b652a490c8db30c8b0bfd1825ca6e6ba9
theory friction practice <jackson.brebnor@gmail.com>
2016-12-16 12:51:39 -0500

    updtes movies array with serch results

--- src/imports/modules/search/actions.js
+++ src/imports/modules/search/actions.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
+import { StateUtils, } from 'imports/utils';
 import { insertMovies, } from '../movies/actions';
 import { SEARCH_URL, UPDATE_SEARCH_RESULTS, } from './constants';
 
-import { StateUtils, } from 'imports/utils';
 const { requestUtils: { requestCreators, }, } = StateUtils;
+const { arrayUtils: { update, }, } = StateUtils;
 
 const searchRequestPending = requestCreators('SEARCH_REQUEST').pending;
 const searchRequestFailure = requestCreators('SEARCH_REQUEST').failure;
 const searchRequestSuccess = requestCreators('SEARCH_REQUEST').success;
-const { arrayUtils: { update, }, } = StateUtils;
 
 const updateResults = results =>
  ({ type: UPDATE_SEARCH_RESULTS, curry: update(results), });
@@ -17,11 +17,9 @@ export const search = ({ query, }) => (dispatch) => {
   dispatch(searchRequestPending(query));
   return axios.get(SEARCH_URL, { params: { query, append_to_response: 'images', }, })
     .then(({ data: { results, }, }) =>
-    [searchRequestSuccess(),
+    [ searchRequestSuccess(),
       updateResults(results),
-      insertMovies(results),].map(dispatch)
-
-      // dispatch(searchRequestSuccess()) && dispatch(updateResults(results))
+      insertMovies(results), ].map(dispatch)
     )
     .catch(searchRequestFailure);
 };
