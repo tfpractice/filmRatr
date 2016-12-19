03478a96d351aa1cf5fac1cb6db9909d1670f39a
theory friction practice <jackson.brebnor@gmail.com>
2016-12-19 17:05:14 -0500

    component clear

--- src/imports/components/movie/top_ten.js
+++ src/imports/components/movie/top_ten.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { bindActionCreators, } from 'redux';
 import { connect, } from 'react-redux';
 import { MovieActions, } from 'imports/actions';
-import { MovieCard, } from '../movie';
 import MovieList from './list';
 
 const mapStateToProps = (state, own) => {
   const movies = state.movies.data;
-
+  
   // console.log('\n===================state.movies.data===================\n', movies);
-
+  
   return { movies, };
 };
 
@@ -17,6 +15,6 @@ const TopTen = ({ movies, actions, }) => (
   <MovieList movies={movies} />
   );
 
-TopTen.fetchData = [ MovieActions.getTopFive, ];
+TopTen.fetchData = [MovieActions.getTopFive,];
 
 export default connect(mapStateToProps)(TopTen);
