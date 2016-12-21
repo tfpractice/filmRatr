cc74bfdd9f282da1b5228003b1ba68247542730b
theory friction practice <jackson.brebnor@gmail.com>
2016-12-20 20:58:58 -0500

    palces nav in sidebar

--- src/imports/components/main.js
+++ src/imports/components/main.js
@@ -2,7 +2,6 @@ import React, { Component, PropTypes, } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import Nav from './nav';
-import SideBar from './sidebar';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
@@ -16,7 +15,6 @@ class Main extends Component {
       <MuiThemeProvider muiTheme={getMuiTheme({ userAgent: false, })}>
         <div id="main-view" className="container">
           <Nav />
-          <SideBar />
           {children}
         </div>
       </MuiThemeProvider>
--- src/imports/components/nav.js
+++ src/imports/components/nav.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
-import { IndependentSearch, } from './search';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import { Link, } from 'react-router';
+import SideBar from './sidebar';
+import { IndependentSearch, } from './search';
 
+const AuthMenu = props => (
+  <IconMenu
+    {...props}
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{ horizontal: 'right', vertical: 'top', }}
+    anchorOrigin={{ horizontal: 'right', vertical: 'top', }}
+  >
+    <MenuItem primaryText="Login" />
+    <MenuItem primaryText="Register" />
+    <MenuItem primaryText="Logout" />
+
+  </IconMenu>
+);
 const Nav = () => (
   <AppBar
     title={<Link to="/" >FilmRatr</Link>}
     iconClassNameRight="muidocs-icon-navigation-expand-more"
+    iconElementLeft={<AuthMenu />}
   >
+    <SideBar />
     <IndependentSearch formID={'navSearchForm'} />
   </AppBar>
 );
