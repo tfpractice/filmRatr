68794f945b4156f8890a230e1bddf2ac61d26401
theory friction practice <jackson.brebnor@gmail.com>
2016-12-21 19:57:05 -0500

    refactors sidebar drawer

--- src/imports/components/shared/sidebar/drawer.js
+++ src/imports/components/shared/sidebar/drawer.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import Drawer as MuiDrawer from 'material-ui/Drawer';
+import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import { local, } from 'redux-fractal';
+import { combineReducers, createStore, } from 'redux';
 
- const Drawer = ({open, trigger, children}) => (
-  <MuiDrawer docked={false}  width={200}  open={open} >
+const toggle = () => ({ type: 'TOGGLE_DRAWER', });
+const open = (state = false, { type, }) =>
+  type === 'TOGGLE_DRAWER' ? !state : state;
+
+const uiProps = {
+  key: 'sidebar',
+  createStore: props => createStore(combineReducers({ open, })),
+  mapDispatchToProps: { toggle, },
+  filterGlobalActions: ({ type, }) => type === 'TOGGLE_DRAWER',
+};
+
+const SideBar = ({ open, toggle, children, }) => (
+  <Drawer docked={false} width={200} open={open} onRequestChange={toggle}>
     {children}
-  </MuiDrawer>
-);
-export default Drawer;
+    <MenuItem primaryText="close sidebar" onTouchTap={toggle} />
+  </Drawer>
+  );
+
+export default local(uiProps)(SideBar);
