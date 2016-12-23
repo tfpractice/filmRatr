5b41fb2146c2d8a664a47f2d0f841ffcfc9e2426
theory friction practice <jackson.brebnor@gmail.com>
2016-12-22 21:43:21 -0500

    refactor search actions

--- src/imports/modules/search/actions.js
+++ src/imports/modules/search/actions.js
@@ -2,27 +2,31 @@ import axios from 'axios';
 import { StateUtils, } from 'imports/utils';
 import { insertMovies, } from '../movies/actions';
 import { getMovieReviews, } from '../reviews/actions';
-
 import { SEARCH_URL, UPDATE_SEARCH_RESULTS, } from './constants';
-const { arrayUtils: { merge, }, } = StateUtils;
-const { requestUtils: { requestCreators, }, } = StateUtils;
-const { dedupe: { getFirst, unaryMap, diff, keySet, }, } = StateUtils;
+
+const { requestUtils: { requestCreators, getData, }, } = StateUtils;
+const { dedupe: { keySet, }, } = StateUtils;
 
 const searchRequestPending = requestCreators('SEARCH_REQUEST').pending;
 const searchRequestFailure = requestCreators('SEARCH_REQUEST').failure;
 const searchRequestSuccess = requestCreators('SEARCH_REQUEST').success;
 
+const tapResults = ({ results, }) => results;
 const updateResults = (...results) =>
  ({ type: UPDATE_SEARCH_RESULTS, curry: merge(...results), });
 
-export const search = ({ query, }) => (dispatch) => {
-  dispatch(searchRequestPending(query));
-  return axios.get(SEARCH_URL, { params: { query, append_to_response: 'images', }, })
-    .then(({ data: { results, }, }) =>
-    Promise.all(
-    [ searchRequestSuccess(),
-      updateResults(...results),
-      insertMovies(...results),
-      getMovieReviews(...keySet(results)), ].map(dispatch)))
-    .catch(err => dispatch(searchRequestFailure(err)));
-};
+export const search = ({ query, }) => dispatch =>
+  Promise.resolve(dispatch(searchRequestPending(query)))
+    .then(() =>
+      axios.get(SEARCH_URL, { params: { query, append_to_response: 'images', }, })
+        .then(getData)
+        .then(tapResults)
+        .then(results => Promise.all(
+      [ searchRequestSuccess(),
+        updateResults(...results),
+        insertMovies(...results),
+        getMovieReviews(...keySet(results)), ].map(dispatch))
+          .then(() => results)))
+    .catch(e => dispatch(searchRequestFailure(e.message)))
+
+;
