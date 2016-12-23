85186d752c1b4df9380a7aeaa8e4a002908ffc31
theory friction practice <jackson.brebnor@gmail.com>
2016-12-22 19:50:53 -0500

    successfully renders deletBUtton in edit form

--- src/imports/components/review/edit_review.js
+++ src/imports/components/review/edit_review.js
@@ -5,20 +5,15 @@ import { resetForm, } from 'imports/utils';
 import { ReviewActions, } from 'imports/actions';
 import ReviewForm from './form_base';
 
-const formStyles = { display: 'flex', 'align-items': 'baseline', };
-
-const EditReviewForm = ({ editReview, review, deleteReview, }) => (
-  <div style={formStyles}>
-    <ReviewForm
-      form={`editReview_${review.id}`}
-      review={review}
-      oprop="oprop"
-      initialValues={review}
-      enableReinitialize
-      onSubmit={editReview(review)}
-      onSubmitSuccess={resetForm(`editReview_${review.id}`)}
-    />
-    <FlatButton secondary label="Delete" onClick={() => deleteReview(review)} />
-  </div>);
+const EditReviewForm = ({ editReview, review, }) => (
+  <ReviewForm
+    form={`editReview_${review.id}`}
+    review={review}
+    enableReinitialize
+    initialValues={review}
+    onSubmit={editReview(review)}
+    onSubmitSuccess={resetForm(`editReview_${review.id}`)}
+  />
+);
 
 export default connect(null, ReviewActions)(EditReviewForm);
--- src/imports/components/review/form_base.js
+++ src/imports/components/review/form_base.js
@@ -1,22 +1,19 @@
-import React, { PropTypes, } from 'react';
+import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
-import { Field, reduxForm, } from 'redux-form';
 import { connect, } from 'react-redux';
 import { TextField, } from 'redux-form-material-ui';
+import { Field, reduxForm, } from 'redux-form';
+import { ReviewActions, } from 'imports/actions';
 
-const mapStateToProps = (s, own) => {
-  console.log('reviewown', own);
-  return ({ myProp: 'myProp', });
-};
+const renderDelete = handler => rev =>
+  rev && <FlatButton secondary label="Delete" onClick={() => handler(rev)} />;
 
-const ReviewForm = ({ handleSubmit, reset, ...props }) => {
-  console.log('reviewProps', props);
-  return (
-    <form onSubmit={handleSubmit} >
-      <Field name="text" component={TextField} hintText="review text" id="text" type="text" />
-      <FlatButton label="Submit" primary type="submit" />
-    </form>
+const ReviewForm = ({ review, handleSubmit, deleteReview, }) => (
+  <form onSubmit={handleSubmit} >
+    <Field name="text" component={TextField} hintText="content" />
+    <FlatButton primary label="Submit" type="submit" />
+    {renderDelete(deleteReview)(review) }
+  </form>
   );
-};
 
-export default connect(mapStateToProps)(reduxForm()(ReviewForm));
+export default connect(null, ReviewActions)(reduxForm()(ReviewForm));
