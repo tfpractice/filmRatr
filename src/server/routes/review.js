1be50840582bc2dac44f3e02626a543d5e31a024
theory friction practice <jackson.brebnor@gmail.com>
2016-12-24 22:03:40 -0500

    sucessfl route protection

--- src/server/routes/review.js
+++ src/server/routes/review.js
@@ -1,5 +1,6 @@
 import { Router, } from 'express';
 import { MovieController, ReviewController, } from '../controllers';
+import { isLoggedIn, } from './auth';
 
 const router = new Router();
 
@@ -13,16 +14,16 @@ router.route('/reviews/').get(ReviewController.getReviews);
 
 router.route('/reviews/:movie_id').get(ReviewController.getMovieReviews);
 
-// Add a new Review
-router.route('/reviews/:movie_id').post(ReviewController.addReview);
-
 // Get one review by cuid
 router.route('/reviews/:movie_id/:id').get(ReviewController.getReview);
 
+// Add a new Review
+router.route('/reviews/:movie_id').post(isLoggedIn, ReviewController.addReview);
+
 // Update one review by cuid
-router.route('/reviews/:movie_id/:id').patch(ReviewController.updateReview);
+router.route('/reviews/:movie_id/:id').patch(isLoggedIn, ReviewController.updateReview);
 
 // Delete a review by cuid
-router.route('/reviews/:movie_id/:id').delete(ReviewController.deleteReview);
+router.route('/reviews/:movie_id/:id').delete(isLoggedIn, ReviewController.deleteReview);
 
 export default router;
