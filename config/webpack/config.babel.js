11d39ee0741b47f46bfec79df9bc4cb724048777
theory friction practice <jackson.brebnor@gmail.com>
2016-12-27 20:32:25 -0500

    removse logs from travis

--- config/webpack/config.babel.js
+++ config/webpack/config.babel.js
@@ -2,11 +2,7 @@ import shared from './shared';
 import * as actions from './actions';
 import { CONFIG_EVENTS, } from './constants';
 
-const config = (common = shared(), event) => {
-  console.log(process.env.MOVIE_DB_API_KEY);
-  console.log(process.env);
-
-  return CONFIG_EVENTS.has(event) ? actions[event](common) : common;
-};
+const config = (common = shared(), event) =>
+CONFIG_EVENTS.has(event) ? actions[event](common) : common;
 
 export default env => (config(shared(env), process.env.npm_lifecycle_event));
