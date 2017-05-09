7448fabff6966291719721ac0f6e314c66f51eaf
theory friction practice <jackson.brebnor@gmail.com>
2017-05-09 18:44:23 -0400

    fixes public path server js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,3 @@
 require('babel-core/register')({});
 require('babel-polyfill');
-
-const { app, } = require('./server');
-
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
-});
-
-// if(module.hot) {
-//     // accept update of dependency
-//     module.hot.accept("./handler.js", function() {
-//         // replace request handler of server
-//         server.removeListener("request", requestHandler);
-//         requestHandler = require("./handler.js");
-//         server.on("request", requestHandler);
-//     });
-// }
+require('./server');
