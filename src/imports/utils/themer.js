275dd5586d98d54f9a2dcc0aca16d07e5d27d74e
theory friction practice <jackson.brebnor@gmail.com>
2017-05-11 11:05:50 -0400

    reinstates stylemamgenr for server rendering

--- src/imports/utils/themer.js
+++ src/imports/utils/themer.js
@@ -1,13 +1,7 @@
-import createPalette, { dark, } from 'material-ui/styles/palette';
+import createPalette from 'material-ui/styles/palette';
 import { createMuiTheme, MuiThemeProvider, } from 'material-ui/styles';
 import { pink, teal, } from 'material-ui/styles/colors';
-
-const palette = createPalette({
-  primary: teal,
-  accent: pink,
-  type: 'dark',
-  ...dark,
-});
+const palette = createPalette({ primary: teal, accent: pink, type: 'dark', });
 
 const { styleManager, theme, } = MuiThemeProvider.createDefaultContext(
   { theme: createMuiTheme({ palette, }), });
