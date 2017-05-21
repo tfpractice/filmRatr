3fcf4b5c54c55011089fc1422d98948b04e74c63
theory friction practice <jackson.brebnor@gmail.com>
2017-05-21 17:56:15 -0400

    about route

--- src/imports/routes.js
+++ src/imports/routes.js
@@ -1,14 +1,16 @@
 import { routes as search, } from './components/search';
 import { routes as movies, } from './components/movie';
+import { routes as home, } from './components/home';
 
-import { Home, Main, } from './components';
-import { MovieActions, } from './actions';
+import { Main, } from './components';
 
-const home = {
-  path: '/',
-  component: Home,
-  loadData: [ MovieActions.getByFreq, ],
-};
+// import { MovieActions, } from './actions';
+//
+// const home = {
+//   path: '/',
+//   component: Home,
+//   loadData: [ MovieActions.getByFreq, ],
+// };
 
 const rootRoute = {
   component: Main,
