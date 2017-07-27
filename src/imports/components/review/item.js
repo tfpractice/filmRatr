1ab61d22fe6eb113ac3924a04ff02285ecf9b543
theory friction practice <jackson.brebnor@gmail.com>
2017-07-26 20:54:32 -0400

    reviewList

--- src/imports/components/review/item.js
+++ src/imports/components/review/item.js
@@ -3,32 +3,37 @@ import Icon from 'material-ui/Icon';
 import Button from 'material-ui/Button';
 import Text from 'material-ui/Typography';
 import StarIcon from 'material-ui-icons/Star';
-import Card, { CardActions, CardContent, CardHeader, } from 'material-ui/Card';
-import { connect, } from 'react-redux';
-import { ReviewActions, } from 'imports/actions';
+import Card, { CardActions, CardContent, CardHeader } from 'material-ui/Card';
+import { connect } from 'react-redux';
+import { ReviewActions } from 'imports/actions';
 import EditReviewForm from './edit_review';
 
-const stars = rating => [ ...Array(rating).keys(), ].map(x => <StarIcon key={x}/>);
+const stars = rating =>
+  [ ...Array(rating).keys() ].map(x => <StarIcon key={x} />);
 
-const mapStateToProps = ({ auth: { user, }, }, { review, }) => ({ canEdit: !!user && user.id === review.user, });
+const mapStateToProps = ({ auth: { user }}, { review }) => ({ canEdit: !!user && user.id === review.user });
 
-const ReviewCard = ({ review, deleteReview, canEdit, }) => (
-  <Card raised>
+const ReviewCard = ({ review, deleteReview, canEdit }) =>
+  (<Card raised>
     <CardHeader
-  title={
-  <Text
-  align="center"
-  gutterBottom
-  type="title">{review.user ? review.user.username : 'anonymous'} || {stars(review.rating)}
-        </Text>}
-  subheader={
-  <Text color='secondary' type="caption">{review.dateAdded} </Text>}
-  />
-    <CardContent >
-      <Text type="body1">{review.text}</Text>
-      {canEdit && <EditReviewForm review={review} /> }
+      title={
+        <Text align="center" gutterBottom type="title">
+          {review.user ? review.user.username : 'anonymous'} ||{' '}
+          {stars(review.rating)}
+        </Text>
+      }
+      subheader={
+        <Text color="secondary" type="caption">
+          {review.dateAdded}{' '}
+        </Text>
+      }
+    />
+    <CardContent>
+      <Text type="body1">
+        {review.text}
+      </Text>
+      {canEdit && <EditReviewForm review={review} />}
     </CardContent>
-  </Card>
-);
+  </Card>);
 
 export default connect(mapStateToProps, ReviewActions)(ReviewCard);
--- src/imports/components/review/list.js
+++ src/imports/components/review/list.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import { connect, } from 'react-redux';
+import { connect } from 'react-redux';
 import Grid from 'material-ui/Grid';
 import Text from 'material-ui/Typography';
 import ReviewCard from './item';
 import MovieReviewForm from './form';
 
-const mapStateToProps = ({ reviews, }, { movie, }) =>
-  ({ reviews: reviews.data.filter(r => r.movie_id == movie.id), });
+const sameID = ({ id }) => ({ movie_id }) => id == movie_id;
+const mapStateToProps = ({ reviews: { data }}, { movie }) => ({ reviews: data.filter(sameID(movie)) });
 
-const ReviewList = ({ movie, reviews, }) => (
-  <Grid container justify="center" direction="column" className="Review-list">
+const ReviewList = ({ movie, reviews }) =>
+  (<Grid container justify="center" align="center" className="Review-list">
     <Grid item xs={6}>
-      <Text type="headline">{`Create Review for ${movie.title}`} </Text>
+      <Text type="headline">
+        {`Create Review for ${movie.title}`}
+      </Text>
       <MovieReviewForm movie={movie} formID={`newReview${movie.id}`} />
     </Grid>
     <Grid item xs={12} className="ReviewList">
-      <Grid container>
-        {reviews.map(r => (<Grid item key={r.id} >
-          <ReviewCard review={r} />
-        </Grid>))}
+      <Grid container justify="center" align="center">
+        {reviews.map(r =>
+          (<Grid item xs={12} sm={6} md={4} key={r.id}>
+            <ReviewCard review={r} />
+          </Grid>)
+        )}
       </Grid>
     </Grid>
-
-  </Grid>
-);
+  </Grid>);
 
 export default connect(mapStateToProps)(ReviewList);
