68f8aac2f9fd21b385178a897d152d983208c0ab
theory friction practice <jackson.brebnor@gmail.com>
2017-07-26 20:44:27 -0400

    movie cnif babel

--- src/imports/components/movie/index.js
+++ src/imports/components/movie/index.js
@@ -1,7 +1,7 @@
-export { default, } from './main';
-export { default as MovieRoute, } from './main';
-export { default as MovieCard, } from './single';
-export { default as MovieView, } from './view';
-export { default as TopTen, } from './top_ten';
-export { default as MovieList, } from './list';
-export { default as routes, } from './config';
+export { default } from './main';
+export { default as MovieRoute } from './main';
+export { default as MovieCard } from './single';
+export { default as MovieView } from './view';
+export { default as TopTen } from './top_ten';
+export { default as MovieList } from './list';
+export { default as routes } from './config';
